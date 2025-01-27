2878c77540cfe5e85508d39441e7f6956a0d874b
Tommy Saysombath <tsays92@yahoo.com>
2025-01-27 03:14:29 +0000

    Testing Mon Jan 27 2025 03:14:29 GMT+0000 (Coordinated Universal Time)

--- chapter1/ex02/student/Lyrics.cs
+++ chapter1/ex02/student/Lyrics.cs
@@ -6,5 +6,9 @@ class Lyrics
 	static void Main()
 	{
 		// Write your code here
+		WriteLine("Well, sad, small, sweet, so delicate");
+		WriteLine("It used to be this dying breed");
+		WriteLine("Well, I've got a bad feeling about this");
+		WriteLine("I've got a bad feeling about this");
 	}
 }
