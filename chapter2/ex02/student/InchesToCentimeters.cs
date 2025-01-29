5840c10016ce48d4bb9ddabc00839a666f1497a5
Tommy Saysombath <tsays92@yahoo.com>
2025-01-29 10:27:45 +0000

    Testing Wed Jan 29 2025 10:27:45 GMT+0000 (Coordinated Universal Time)

--- chapter2/ex02/student/InchesToCentimeters.cs
+++ chapter2/ex02/student/InchesToCentimeters.cs
@@ -5,6 +5,10 @@ class InchesToCentimeters
 {
 	static void Main()
 	{
-		// Write your code here
+		const double cmToInch = 2.54;
+		double inches = 3;
+		double totalCm = inches * cmToInch;
+		WriteLine("The length of {0} inches being converted using {1} centimeters per inch is equal to {2} centimeters", inches, cmToInch ,totalCm);
+
 	}
 }
