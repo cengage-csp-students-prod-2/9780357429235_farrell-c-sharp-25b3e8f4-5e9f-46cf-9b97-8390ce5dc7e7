29a7c1ef52e14d51a08fb76c5ecafb4759a9989e
Tommy Saysombath <tsays92@yahoo.com>
2025-01-29 10:02:45 +0000

    Testing Wed Jan 29 2025 10:02:45 GMT+0000 (Coordinated Universal Time)

--- chapter2/ex01/student/DoubleDecimalTest.cs
+++ chapter2/ex01/student/DoubleDecimalTest.cs
@@ -5,6 +5,10 @@ class DoubleDecimalTest
 {
 	static void Main()
 	{
-		// Write your code here
+		double largeDouble = 1239.293949322939571890374589123470129570892357102395701570193190472103784556413248946531986451
+		decimal largeDecimial = 1239.293949322939571890374589123470129570892357102395701570193190472103784556413248946531986451
+
+		WriteLine("Double number" + largeDouble);
+		WriteLine("Decimal number" + largeDecimial);
 	}
 }
