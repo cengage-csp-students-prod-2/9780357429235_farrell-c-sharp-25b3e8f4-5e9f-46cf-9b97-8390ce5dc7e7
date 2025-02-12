f08c593092144286d297160d54a7a6c31003ed76
Tommy Saysombath <tsays92@yahoo.com>
2025-02-12 19:56:15 +0000

    Testing Wed Feb 12 2025 19:56:15 GMT+0000 (Coordinated Universal Time)

--- chapter4/ex03/student/Admission.cs
+++ chapter4/ex03/student/Admission.cs
@@ -6,5 +6,17 @@ class Admission
 	static void Main()
 	{
 		// Write your code here
+		string gradePointString, testScoreString;
+		double gradePoint, testScore;
+		Write("Please enter your GPA: ");
+		gradePointString = ReadLine();
+		gradePoint = Convert.ToDouble(gradePointString);
+		Write("Please enter your admission test score: ");
+		testScoreString = ReadLine();
+		testScore = Convert.ToDouble(testScoreString);
+		if ((gradePoint >= 3.0 && testScore >= 60) || (gradePoint < 3.0 && testScore >= 80))
+			Console.WriteLine("Accept");
+			else
+			 Console.WriteLine("Reject");
 	}
 }
