6a56e4b61c714a2d368f7f8c9f6b5917eca4b386
Tommy Saysombath <tsays92@yahoo.com>
2025-02-12 22:24:49 +0000

    Testing Wed Feb 12 2025 22:24:49 GMT+0000 (Coordinated Universal Time)

--- chapter4/ex06/student/CheckMonth2.cs
+++ chapter4/ex06/student/CheckMonth2.cs
@@ -3,8 +3,39 @@ using static System.Console;
 using System.Globalization;
 class CheckMonth2
 {
-	static void Main()
-	{
-		// Write your code here
-	}
+    static void Main()
+    {
+        // Write your code here
+
+        // This gives a # of days for each month
+        int[] daysInMonth = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+        Console.Write("Enter your birth month (1-12)");
+        int month = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Enter your birthday (1-31)");
+        int day = Convert.ToInt32(Console.ReadLine());
+
+        //This helps keep the months valid
+        if (month < 1 || month > 12)
+        {
+            Console.WriteLine("Invalid date");
+        }
+        //Checks for leap year
+        if (month == 2)
+        {
+            Console.Write("Enter the year to check for leap year: ");
+            int year = Convert.ToInt32(Console.ReadLine());
+            if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
+            {
+                daysInMonth[2] = 29;
+            }
+        }
+        if (day < 1 || day > daysInMonth[month])
+        {
+            Console.WriteLine("Invalid date");
+        }
+        else
+        {
+            Console.WriteLine($"{month}/{day} is a valid birthday.");
+        }
+    }
 }
