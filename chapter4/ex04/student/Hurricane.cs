a02174d3ecb8a0586a906139c9ae7b130e37bbc0
Tommy Saysombath <tsays92@yahoo.com>
2025-02-12 20:07:24 +0000

    Testing Wed Feb 12 2025 20:07:24 GMT+0000 (Coordinated Universal Time)

--- chapter4/ex04/student/Hurricane.cs
+++ chapter4/ex04/student/Hurricane.cs
@@ -6,5 +6,34 @@ class Hurricane
 	static void Main()
 	{
 		// Write your code here
+		int windSpeed;
+		string windSpeedString;
+		Write("Enter the wind speed (mph)");
+		windSpeedString = ReadLine();
+		windSpeed = Convert.ToInt32(windSpeedString);
+		if (windSpeed >= 157)
+		{
+			WriteLine("This is a category 5 hurricane.");
+		}
+		else if (windSpeed >= 130)
+		{
+			WriteLine("This is a category 4 hurricane.");
+		}
+		else if (windSpeed >= 111)
+		{
+			WriteLine("This is a category 3 hurricane.");
+		}
+		else if (windSpeed >= 96)
+		{
+			WriteLine("This is a category 2 hurricane.");
+		}
+		else if (windSpeed >= 74)
+		{
+			WriteLine("This is a category 1 hurricane.");
+		}
+		else
+		{
+			WriteLine("This is not a hurricane.");
+		}
 	}
 }
