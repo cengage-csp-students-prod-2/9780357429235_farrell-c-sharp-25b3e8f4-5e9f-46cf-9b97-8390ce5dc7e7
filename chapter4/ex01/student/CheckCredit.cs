a35e6605c8e239cdfbbd73c5faca6b10ad16f349
Tommy Saysombath <tsays92@yahoo.com>
2025-02-12 19:27:40 +0000

    Testing Wed Feb 12 2025 19:27:40 GMT+0000 (Coordinated Universal Time)

--- chapter4/ex01/student/CheckCredit.cs
+++ chapter4/ex01/student/CheckCredit.cs
@@ -6,5 +6,16 @@ class CheckCredit
 	static void Main()
 	{
 		// Write your code here
+		const double CREDIT_LIMIT = 8000.0;
+		string priceString;
+		double price;
+		Write("Please enter the purchase price: ");
+		priceString = ReadLine();
+		price = Convert.ToDouble(priceString);
+		if (price > CREDIT_LIMIT)
+			WriteLine("You have exceeded the credit limit.");
+		else
+			WriteLine("Approved.");
+		
 	}
 }
