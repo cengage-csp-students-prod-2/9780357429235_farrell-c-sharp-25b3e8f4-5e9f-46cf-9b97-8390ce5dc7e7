561e5937728fe189e0e8f40d3429bafa38ce1fde
Tommy Saysombath <tsays92@yahoo.com>
2025-02-12 19:38:53 +0000

    Testing Wed Feb 12 2025 19:38:53 GMT+0000 (Coordinated Universal Time)

--- chapter4/ex02/student/Twitter.cs
+++ chapter4/ex02/student/Twitter.cs
@@ -6,5 +6,16 @@ class Twitter
 	static void Main()
 	{
 		// Write your code here
+		const int TEXT_LENGTH = 140;
+		Console.Write("Enter your message: ");
+		string message = Console.ReadLine();
+		if (message.Length > TEXT_LENGTH)
+		{
+			Console.WriteLine("This message is too long.");
+		}
+		else
+		{
+			Console.WriteLine("This message is okay.");
+		}
 	}
 }
