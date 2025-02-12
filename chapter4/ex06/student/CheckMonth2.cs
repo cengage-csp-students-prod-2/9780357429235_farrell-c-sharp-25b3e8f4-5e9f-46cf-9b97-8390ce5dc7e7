e115601213a65955fc98bbbf7f2e201ea245261b
Tommy Saysombath <tsays92@yahoo.com>
2025-02-12 22:39:28 +0000

    Testing Wed Feb 12 2025 22:39:28 GMT+0000 (Coordinated Universal Time)

--- chapter4/ex06/student/CheckMonth2.cs
+++ chapter4/ex06/student/CheckMonth2.cs
@@ -6,15 +6,12 @@ class CheckMonth2
     static void Main()
     {
         // Write your code here
-
-        // This gives a # of days for each month
         int[] daysInMonth = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
         Console.Write("Enter your birth month (1-12)");
         int month = Convert.ToInt32(Console.ReadLine());
         Console.Write("Enter your birthday (1-31)");
         int day = Convert.ToInt32(Console.ReadLine());
 
-        //This helps keep the months valid
         if (month < 1 || month > 12)
         {
             Console.WriteLine("Invalid date");
