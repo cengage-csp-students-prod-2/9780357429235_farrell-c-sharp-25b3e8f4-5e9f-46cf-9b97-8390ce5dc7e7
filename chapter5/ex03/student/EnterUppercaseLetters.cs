7f1fee56d425cd965b7a8634ac5e1f13f5e0a87a
Tommy Saysombath <tsays92@yahoo.com>
2025-02-20 04:21:03 +0000

    Testing Thu Feb 20 2025 04:21:03 GMT+0000 (Coordinated Universal Time)

--- chapter5/ex03/student/EnterUppercaseLetters.cs
+++ chapter5/ex03/student/EnterUppercaseLetters.cs
@@ -6,5 +6,28 @@ class EnterUppercaseLetters
 	static void Main()
 	{
 		// Write your code here
+		char inputCharacter;
+		do
+		{
+			Write("Enter an uppercase letter or enter '!' to quit: ");
+			string input = ReadLine();
+			if (string.IsNullOrEmpty(input))
+			{
+				inputCharacter = ' ';
+				continue;
+			}
+			inputCharacter = input [0];
+			if (inputCharacter != '!')
+			{
+				if (char.IsUpper(inputCharacter))
+				{
+					WriteLine("OK");
+				}
+				else
+				{
+					WriteLine("Sorry - that was not an uppercase letter");
+				}
+			}
+		} while(inputCharacter != '!');
 	}
 }
