5e0bef82605d11a58b8ff22136b355320c3c4715
Tommy Saysombath <tsays92@yahoo.com>
2025-02-20 04:05:55 +0000

    Testing Thu Feb 20 2025 04:05:55 GMT+0000 (Coordinated Universal Time)

--- chapter5/ex02/student/SumInts.cs
+++ chapter5/ex02/student/SumInts.cs
@@ -6,5 +6,16 @@ class SumInts
 	static void Main()
 	{
 		// Write your code here
+		int sum = 0;
+		int input;
+		Write("Enter intergers (enter 999 to stop and add the total up):");
+		input = Convert.ToInt32(ReadLine());
+		while(input != 999)
+		{
+			sum += input;
+			Write("Enter intergers (enter 999 to stop and add the total up):");
+			input = Convert.ToInt32(ReadLine());
+		}
+		WriteLine("The total values entered is " + sum);
 	}
 }
