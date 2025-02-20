216f91f0f64153149c52be5f2f9f9381473ccfbe
Tommy Saysombath <tsays92@yahoo.com>
2025-02-20 03:45:52 +0000

    Testing Thu Feb 20 2025 03:45:52 GMT+0000 (Coordinated Universal Time)

--- chapter5/ex01/student/SumFiveInts.cs
+++ chapter5/ex01/student/SumFiveInts.cs
@@ -6,5 +6,15 @@ class SumFiveInts
 	static void Main()
 	{
 		// Write your code here
+		int sum = 0;
+		WriteLine("Enter 5 whole numbers: ");
+
+		for (int x = 0; x < 5; x++)
+		{
+			Write($"Enter number {x + 1}: ");
+			int number = Convert.ToInt32(ReadLine());
+			sum += number;
+		}
+		WriteLine("The sum of the 5 numbers is {0}.", sum);
 	}
 }
