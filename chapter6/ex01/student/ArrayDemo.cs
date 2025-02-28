8676ff6e117ff4b259f6bfb8fec2254aeb6dd91b
Tommy Saysombath <tsays92@yahoo.com>
2025-02-28 08:35:59 +0000

    Testing Fri Feb 28 2025 08:35:59 GMT+0000 (Coordinated Universal Time)

--- chapter6/ex01/student/ArrayDemo.cs
+++ chapter6/ex01/student/ArrayDemo.cs
@@ -11,5 +11,51 @@ class ArrayDemo
 	{
 		int[] nums = {7, 6, 3, 2, 10, 8, 4, 5, 9, 1};
 		// Write your code here
+		int options;
+		WriteLine("1 - View the list in order from the first to last position in the stored array.");
+		WriteLine("2 - View the list in order from the last to first position.");
+		WriteLine("3 - Choose a specific position to view.");
+		WriteLine("4 - Quit the application.");
+		Write("Enter which option you would like to select: ");
+		options = Convert.ToInt32(ReadLine());
+
+		switch (options)
+		{
+			case 1:
+				WriteLine("From first to fast:");
+				Array.Sort(nums);
+				for (int x = 0; x < nums.Length; x++)
+				{
+					Write(nums[x] + " ");
+				}
+				WriteLine();
+				break;
+			case 2:
+				WriteLine("From last to first:");
+				Array.Sort(nums);
+				Array.Reverse(nums);
+				for (int x = 0; x < nums.Length; x++)
+				{
+					Write(nums[x] + " ");
+				}
+				WriteLine();
+				break;
+			case 3:
+				WriteLine("Enter a position (0-9): ");
+				int position = Convert.ToInt32(ReadLine());
+				if (position >= 0 && position < nums.Length)
+				{
+					WriteLine("The value at position {0} is {1}.", position, nums[position]);
+				}
+				else
+				{
+					WriteLine("This position is not valid. Please enter a number between 0-9.");
+				}
+				break;
+			case 4:
+				WriteLine("Closing the application");
+				break;
+		}
+
 	}
 }
