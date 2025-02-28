c8b8f1be58301cbb7e2501878923e17938a4e131
Tommy Saysombath <tsays92@yahoo.com>
2025-02-28 09:05:07 +0000

    Testing Fri Feb 28 2025 09:05:07 GMT+0000 (Coordinated Universal Time)

--- chapter6/ex02/student/TestScoreList.cs
+++ chapter6/ex02/student/TestScoreList.cs
@@ -6,5 +6,24 @@ class TestScoreList
 	static void Main()
 	{
 		// Write your code here
+		const int num_scores = 8;
+		int[] scores = new int[num_scores];
+		int sum = 0;
+
+		for (int x = 0; x < num_scores; x++)
+		{
+			Write("Enter your test score for #{0}: ", x);
+			scores[x] = Convert.ToInt32(ReadLine());
+			sum += scores[x];
+		} 
+
+		double mean = (double)sum / num_scores;
+
+		WriteLine("Test scores and the mean:");
+		for (int y = 0; y < num_scores; y++)
+		{
+			int z = scores[y] - (int)mean;
+			WriteLine("Test # {0}: {1,5} From mean: {2,5}", y, scores[y], z);
+		}
 	}
 }
