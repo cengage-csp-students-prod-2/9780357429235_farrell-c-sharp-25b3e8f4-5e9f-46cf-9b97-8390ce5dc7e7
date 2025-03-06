c6bffb2f0b64e597b051389162fef0d04ca506c6
Tommy Saysombath <tsays92@yahoo.com>
2025-03-06 03:57:41 +0000

    Testing Thu Mar 06 2025 03:57:41 GMT+0000 (Coordinated Universal Time)

--- chapter7/ex02/student/PaintingEstimate.cs
+++ chapter7/ex02/student/PaintingEstimate.cs
@@ -5,6 +5,21 @@ class PaintingEstimate
 {
 	static void Main()
 	{
-		// Write your code here
-	}
+        Write("Enter length of the room in feet >> ");
+        double length = Convert.ToDouble(Console.ReadLine());
+        Write("Enter width of the room in feet >> ");
+        double width = Convert.ToDouble(Console.ReadLine());
+        double totalCost = CalculatePaintingCost(length, width);
+        WriteLine("Cost of job for {0} X {1} foot room is {2}",
+            length, width, totalCost.ToString("C", CultureInfo.GetCultureInfo("en-US")));
+    }
+
+    static double CalculatePaintingCost(double length, double width)
+    {
+        const int height = 9; 
+        const double pricePerSquareFoot = 6.0;
+        double wallArea = 2 * height * (length + width);
+        double totalCost = wallArea * pricePerSquareFoot;
+        return totalCost;
+    }
 }
