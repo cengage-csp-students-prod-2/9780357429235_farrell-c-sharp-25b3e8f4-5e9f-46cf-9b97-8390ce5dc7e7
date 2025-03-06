e2a780f439bcc752b55b07b8ccab0a329cbba39f
Tommy Saysombath <tsays92@yahoo.com>
2025-03-06 04:20:54 +0000

    Testing Thu Mar 06 2025 04:20:54 GMT+0000 (Coordinated Universal Time)

--- chapter7/ex03/student/ConvertMilesToKilometers.cs
+++ chapter7/ex03/student/ConvertMilesToKilometers.cs
@@ -5,12 +5,12 @@ class ConvertMilesToKilometers
 {
 	static void Main()
 	{
-		Write("Enter a number of miles");
+		Write("Enter a number of miles: ");
 		double miles = Convert.ToDouble(ReadLine());
 		double kilometers = ConvertToKilometers(miles);
 		WriteLine("{0} miles is {1} kilometers", miles, kilometers);
 	}
-	static double ConvertToKilometers(double miles)
+	public static double ConvertToKilometers(double miles)
 	{
 		const double kilometersPerMile = 1.60934;
 		return miles * kilometersPerMile;
