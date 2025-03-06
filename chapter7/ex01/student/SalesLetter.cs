aa38e31ee6c91c86f9fca4e9073aa7168e06870e
Tommy Saysombath <tsays92@yahoo.com>
2025-03-06 03:22:47 +0000

    Testing Thu Mar 06 2025 03:22:47 GMT+0000 (Coordinated Universal Time)

--- chapter7/ex01/student/SalesLetter.cs
+++ chapter7/ex01/student/SalesLetter.cs
@@ -4,7 +4,27 @@ using System.Globalization;
 class SalesLetter
 {
 	static void Main()
-	{
-		// Write your code here
-	}
+    {
+        Console.WriteLine("From");
+        DisplayContactInfo();
+        Console.WriteLine("\n______________________________\n");
+
+        Console.WriteLine("Dear Client,");
+        Console.WriteLine("We want to provide you good service.");
+        Console.WriteLine("Feel free to contact us at any time.\n");
+
+        DisplayContactInfo();
+        Console.WriteLine("\n********\n");
+
+        Console.WriteLine("Looking forward to a long relationship.\n");
+        DisplayContactInfo();
+    }
+
+    static void DisplayContactInfo()
+    {
+        Console.WriteLine("C# Company");
+        Console.WriteLine("Phone: 555-1234   Cell: 555-0912");
+        Console.WriteLine("Email: csharpcompany@csharp.com");
+        Console.WriteLine("On the Web at www.csharpcompanyforyou.com");
+    }
 }
